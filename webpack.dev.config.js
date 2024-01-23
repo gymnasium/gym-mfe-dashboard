ac25103dd6a409af4645da5f64070e02bbcbbfcb
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-23 10:51:05 -0500

    Update webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,11 +7,12 @@ const config = createConfig('webpack-dev');
 config.resolve.modules = [
   path.resolve(__dirname, './src'),
   'node_modules',
-  path.resolve(__dirname, './node_modules'),
-  path.resolve(__dirname, '../frontend-component-header/src'),
-  path.resolve(__dirname, '../frontend-component-footer/src'),
-  path.resolve(__dirname, '../brand-openedx/src'),
 ];
+
+config.resolve.alias = {
+  '@edx/frontend-component-footer': path.resolve(__dirname, '../frontend-component-footer/src'),
+  '@edx/frontend-component-header': path.resolve(__dirname, '../frontend-component-header/src'),
+};
 
 config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
 
